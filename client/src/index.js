bbcce2cc1d36ad04c7b8ce5d7bb50cdcd0db2066
MarioDiCaprio <mariodicaprio10@gmail.com>
2021-12-24 14:57:33 +0100

    -Replaced CSS with Sass -Added App logo

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import Home from './views/home';
 
 ReactDOM.render(
